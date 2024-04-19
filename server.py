5183b73b287a88b7c4b5b5d68acd3d7875298f2e
Timothy Ober <timothy.ober@gmail.com>
2024-04-18 20:33:15 -0400

    finished task 6

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from EmotionDetection.emotion_detection import emotion_detector
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/emotionDetector/<statement>")
-def show_emotion_detector():
-    return emotion_detector(statement)
+@app.route("/emotionDetector/<string:statement>")
+def show_emotion_detector(statement):
+    resp = json.loads(emotion_detector(statement))
+    return_string = "For the given statement, the system response is" \
+     f"'anger': {resp['anger']}, 'disgust': {resp['disgust']}, " \
+     f"'fear': {resp['fear']}, 'joy': {resp['joy']} and 'sadness': {resp['sadness']}." \
+     f" The dominant emotion is {resp['dominant_emotion']}."
+    return return_string
